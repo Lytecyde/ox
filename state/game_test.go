f4250d66a06ff0d69ccb59f011ec94c33932122f
Tanel Lebedev <tanel.lebedev@gmail.com>
2017-12-02 00:28:47 +0200

    Add test coverage

--- state/game_test.go
+++ state/game_test.go
@@ -5,11 +5,10 @@ import (
 	"time"
 
 	"github.com/Lytecyde/ox/coordinates"
-	"github.com/Lytecyde/ox/settings"
 	"github.com/stretchr/testify/assert"
 )
 
-func Test_NewGameState_SetsMatrixSizeFromParameters(t *testing.T) {
+func Test_NewGameState_SetsMatrixSizeFromParameters_InCaseOfSuccess(t *testing.T) {
 	// Arrange
 	x, y := 100, 100
 
@@ -21,10 +20,10 @@ func Test_NewGameState_SetsMatrixSizeFromParameters(t *testing.T) {
 	assert.Equal(t, y, gameState.Matrix.Dimensions.Y)
 }
 
-func Test_moveCursor_MovesCursorToGivenCoordinates(t *testing.T) {
+func Test_moveCursor_MovesCursorToGivenCoordinates_InCaseOfSuccess(t *testing.T) {
 	// Arrange
 	gameState := NewGame(100, 100)
-	gameState.KeyAt = time.Now().Add(-(settings.HumanReactionSeconds * 1000) * time.Millisecond)
+	gameState.KeyAt = time.Now().Add(-1 * time.Second)
 	c := coordinates.NewMatrix(2, 2)
 
 	// Act
@@ -36,74 +35,86 @@ func Test_moveCursor_MovesCursorToGivenCoordinates(t *testing.T) {
 	assert.Equal(t, c.Y, gameState.Cursor.Y)
 }
 
-func Test_moveCursor_DoesNotMoveCursor_InCaseOfNegativeX(t *testing.T) {
+func Test_moveCursor_ReturnsError_InCaseOfNegativeX(t *testing.T) {
 	// Arrange
 	gameState := NewGame(100, 100)
+	gameState.KeyAt = time.Now().Add(-1 * time.Second)
 	c := coordinates.NewMatrix(-1, 2)
 
 	// Act
-	gameState.moveCursor(c)
+	err := gameState.moveCursor(c)
 
 	// Assert
+	assert.Error(t, err)
+	assert.Equal(t, "x less than 0", err.Error())
 	assert.NotEqual(t, c.X, gameState.Cursor.X)
 	assert.NotEqual(t, c.Y, gameState.Cursor.Y)
 }
 
-func Test_moveCursor_DoesNotMoveCursor_InCaseOfOffScreenX(t *testing.T) {
+func Test_moveCursor_ReturnsError_InCaseOfOffScreenX(t *testing.T) {
 	// Arrange
 	gameState := NewGame(100, 100)
+	gameState.KeyAt = time.Now().Add(-1 * time.Second)
 	c := coordinates.NewMatrix(101, 2)
 
 	// Act
-	gameState.moveCursor(c)
+	err := gameState.moveCursor(c)
 
 	// Assert
+	assert.Error(t, err)
+	assert.Equal(t, "out of the matrix", err.Error())
 	assert.NotEqual(t, c.X, gameState.Cursor.X)
 	assert.NotEqual(t, c.Y, gameState.Cursor.Y)
 }
 
-func Test_moveCursor_DoesNotMoveCursor_InCaseOfNegativeY(t *testing.T) {
+func Test_moveCursor_ReturnsError_InCaseOfNegativeY(t *testing.T) {
 	// Arrange
 	gameState := NewGame(100, 100)
+	gameState.KeyAt = time.Now().Add(-1 * time.Second)
 	c := coordinates.NewMatrix(1, -1)
 
 	// Act
-	gameState.moveCursor(c)
+	err := gameState.moveCursor(c)
 
 	// Assert
+	assert.Error(t, err)
+	assert.Equal(t, "y less than 0", err.Error())
 	assert.NotEqual(t, c.X, gameState.Cursor.X)
 	assert.NotEqual(t, c.Y, gameState.Cursor.Y)
 }
 
-func Test_moveCursor_DoesNotMoveCursor_InCaseOfOffScreenY(t *testing.T) {
+func Test_moveCursor_ReturnsError_InCaseOfOffScreenY(t *testing.T) {
 	// Arrange
 	gameState := NewGame(100, 100)
+	gameState.KeyAt = time.Now().Add(-1 * time.Second)
 	c := coordinates.NewMatrix(1, 101)
 
 	// Act
-	gameState.moveCursor(c)
+	err := gameState.moveCursor(c)
 
 	// Assert
+	assert.Error(t, err)
+	assert.Equal(t, "out of the matrix", err.Error())
 	assert.NotEqual(t, c.X, gameState.Cursor.X)
 	assert.NotEqual(t, c.Y, gameState.Cursor.Y)
 }
 
-func Test_moveCursor_InCaseOfTimeIsWithinHumanPerceptionLimit(t *testing.T) {
+func Test_moveCursor_MovesCursor_InCaseOfSuccess(t *testing.T) {
 	// Arrange
 	gameState := NewGame(10, 10)
-	duration := time.Duration(1 * time.Second)
-	gameState.KeyAt = time.Now().Add(-duration)
+	gameState.KeyAt = time.Now().Add(-1 * time.Second)
 	c := coordinates.NewMatrix(5, 5)
 
 	// Act
-	gameState.moveCursor(c)
+	err := gameState.moveCursor(c)
 
 	// Assert
+	assert.NoError(t, err)
 	assert.Equal(t, c.X, gameState.Cursor.X)
 	assert.Equal(t, c.Y, gameState.Cursor.Y)
 }
 
-func Test_moveCursor_InCaseOfTimeIsOutOfHumanPerceptionLimit(t *testing.T) {
+func Test_moveCursor_ReturnsError_InCaseOfTimeIsOutOfHumanPerceptionLimit(t *testing.T) {
 	// Arrange
 	gameState := NewGame(10, 10)
 	duration := time.Duration(100 * time.Millisecond)
@@ -111,9 +122,11 @@ func Test_moveCursor_InCaseOfTimeIsOutOfHumanPerceptionLimit(t *testing.T) {
 	c := coordinates.NewMatrix(5, 5)
 
 	// Act
-	gameState.moveCursor(c)
+	err := gameState.moveCursor(c)
 
 	// Assert
+	assert.Error(t, err)
+	assert.Equal(t, "less than human reaction time limit", err.Error())
 	assert.NotEqual(t, c.X, gameState.Cursor.X)
 	assert.NotEqual(t, c.Y, gameState.Cursor.Y)
 }
